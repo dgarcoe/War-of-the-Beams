380b3c2f724fdd8849bb12e0560a38c9f46204b6
dgarcoe <dannn86m@gmail.com>
2018-08-13 21:37:35 +0200

    Added initial code

---
+++ android/src/com/dgarcoe/warofthebeams/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.dgarcoe.warofthebeams;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.dgarcoe.warofthebeams.MainGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MainGame(), config);
+	}
+}
---
+++ desktop/src/com/dgarcoe/warofthebeams/desktop/DesktopLauncher.kt
@@ -0,0 +1,16 @@
+package com.dgarcoe.warofthebeams.desktop
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.dgarcoe.warofthebeams.MainGame
+
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = LwjglApplicationConfiguration()
+        config.title = "War of the Beams"
+        config.width = 1024
+        config.height = 768
+        LwjglApplication(MainGame(), config)
+    }
+}
